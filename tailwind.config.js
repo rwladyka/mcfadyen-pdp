0e99cf1372af34f5b6b358f2a3f57358442e66b9
Rodrigo Wladyka <rodrigo.wladyka@gmail.com>
2023-09-17 21:41:08 -0300

    chore: add tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: [],
   theme: {
+    colors: {
+      black: '#000',
+      white: '#FFF',
+      darkGrey: '#767676',
+      grey: '#A6A6A6',
+      lightGrey: '#E5E5E5',
+      offWhite: '#F5F5F5',
+      red: '#CA4747',
+    },
+    fontFamily: {
+      sans: 'Roboto',
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
