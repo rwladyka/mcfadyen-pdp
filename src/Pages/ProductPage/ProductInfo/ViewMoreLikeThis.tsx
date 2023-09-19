e811627ac8dfce94b33be4de9641f50070d2b4ca
Rodrigo Wladyka <rodrigo.wladyka@gmail.com>
2023-09-19 12:01:00 -0300

    fix: add margin-x and font-size

--- src/Pages/ProductPage/ProductInfo/ViewMoreLikeThis.tsx
+++ src/Pages/ProductPage/ProductInfo/ViewMoreLikeThis.tsx
@@ -3,11 +3,11 @@ type ViewMoreLikeThisProps = {
 };
 
 const ViewMoreLikeThis = ({ items }: ViewMoreLikeThisProps) => (
-  <div className="flex justify-center flex-col my-20">
+  <div className="flex justify-center flex-col my-20 mx-[18px]">
     <h2 className="uppercase font-bold text-center">
       view more like this product
     </h2>
-    <ul className="flex justify-center gap-6 mt-3">
+    <ul className="flex justify-center gap-6 mt-3 text-sm">
       {items.map((item) => (
         <li key={item} className="uppercase underline cursor-pointer">
           {item}
